fde2026d8377efdb7a805694db96eced8a535ef0
YoanBst <yoan.bastides@etu.umontpellier.fr>
2025-05-17 09:45:18 +0200

    a

--- front_server.js
+++ front_server.js
@@ -1,25 +1,18 @@
 const express = require('express');
 const path = require('path');
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
 const app = express();
 
 const publicDir = path.join(__dirname, 'public');
 app.use(express.static(publicDir));
 
-// Redirige vers la page de login si la route n'existe pas
+// Si aucune route ne correspond, on renvoie la page login.html
 app.use((req, res) => {
   res.status(404).sendFile(path.join(publicDir, 'HTML', 'login.html'));
 });
 
 const PORT = process.env.PORT || 8000;
 
-// Lis les certificats SSL
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
-
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`Express HTTPS static server running on port ${PORT} for the files in ${publicDir}`);
+http.createServer(app).listen(PORT, () => {
+  console.log(`Express HTTP static server running on port ${PORT} for the files in ${publicDir}`);
 });
