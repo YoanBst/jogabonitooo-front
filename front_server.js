a402b6c2066bec3341fcdbe486903fc78050c372
YoanBst <yoan.bastides@etu.umontpellier.fr>
2025-05-22 17:20:49 +0200

    a

--- front_server.js
+++ front_server.js
@@ -11,7 +11,7 @@ app.use((req, res) => {
   res.status(404).sendFile(path.join(publicDir, 'HTML', 'login.html'));
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 http.createServer(app).listen(PORT, () => {
   console.log(`Express HTTP static server running on port ${PORT} for the files in ${publicDir}`);
